806c8286bfee87879efbe25e7d23f5cf0e3db988
Bodmain <doquan17062004@gmail.com>
2026-03-25 07:47:22 +0700

    notifice update

--- app/src/main/java/com/example/note2/components_ui/HomeTopBar.kt
+++ app/src/main/java/com/example/note2/components_ui/HomeTopBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.note2.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -51,7 +52,9 @@ fun NotesTopBar(
             }
 
             // Thông báo
-            IconButton(onClick = onNotificationClick) {
+            IconButton(
+                onClick = onNotificationClick
+            ) {
                 Icon(
                     imageVector = Icons.Outlined.Notifications,
                     contentDescription = "Notifications",
--- app/src/main/java/com/example/note2/ui_Screen/HomeScreen.kt
+++ app/src/main/java/com/example/note2/ui_Screen/HomeScreen.kt
@@ -1,18 +1,14 @@
-package com.example.note2.ui_note
+package com.example.note2.ui_Screen
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.note2.Screen
 import com.example.note2.components_ui.DeleteConfirmDialog
 import com.example.note2.components_ui.NoteFAB
@@ -26,6 +22,7 @@ import com.example.note2.components_ui.NotesTopBar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
+    navController: NavController,
     viewModel: NoteViewModel,
     onAddNote: () -> Unit,
     onEditNote: (NoteModel) -> Unit,
@@ -39,7 +36,7 @@ fun HomeScreen(
                 NotesTopBar(
                     onToggleSearch = { viewModel.toggleSearch() },
                     onCalendarClick = { /* Handle calendar click */ },
-                    onNotificationClick = { /* Handle notification click */ },
+                    onNotificationClick = {navController.navigate(Screen.Notification.route)},
                     onMenuClick = { onNavigateToProfile() }
                 )
             }
